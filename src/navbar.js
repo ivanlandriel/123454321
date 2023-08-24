f29fc36bd33443147ec0da174c33f0a8887bf92e
ivanlandriel <131979563+ivanlandriel@users.noreply.github.com>
2023-08-24 20:48:29 -0300

    Add files via upload

---
+++ src/navbar.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function NavBar() {
+  return (
+    <nav>
+      <ul>
+        <li><a href="#">Inicio</a></li>
+        <li><a href="#">Acerca de</a></li>
+        <li><a href="#">Servicios</a></li>
+        <li><a href="#">Contacto</a></li>
+      </ul>
+    </nav>
+  );
+}
+
+export default NavBar;
